d99aa73e38b50c3f5b6185cc9330846f3af18469
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2021-12-14 19:42:17 +0100

    create seed data

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground")
+
+mongoose.connect("mongodb://localhost:27017/campground", {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+})
+
+mongoose.connection.on("error", console.error.bind(console, "connection error:"));
+mongoose.connection.once("open", () => {
+  console.log("Database connected")
+})
+
 const app = express();
-const path = require("path")
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"))
 
 app.get("/", (req, res) => {
   res.render("home")
+})
+
+app.get("/campground", async (req, res) => {
+  const camp = new Campground({ 
+    title: "my back yard",
+    description: "affordable camping!"
+ })
+  await camp.save()
+  res.send(camp)
 })
 
 app.listen(3000, () => {
