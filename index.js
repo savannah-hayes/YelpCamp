c8990b60761464e94fc1e13d33efdf4c9979305d
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2021-12-26 20:28:23 +0100

    Create user model and configure passport

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const ExpressError = require("./utilities/ExpressError");
 const methodOverride = require("method-override");
+const passport = require("passport");
+const localStrategy = require("passport-local");
+const User = require("./models/user");
 
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
@@ -41,13 +44,30 @@ const sessionConfig = {
     maxAge: 1000 * 60 * 60 * 24 * 7
   }
 }
+
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
+})
+
+app.get("/fakeUser", async (req, res) => {
+  const user = new User({
+    email: "apple123@yahoo.com",
+    username: "core"
+  })
+  const newUser = await User.register(user, "hunter");
+  res.send(newUser)
 })
 
 app.use("/campgrounds", campgrounds);
