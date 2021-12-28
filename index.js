bdfec433595555c8b1db5c9ac82e733b0beb5053
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2021-12-28 23:18:44 +0100

    Add register form

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const passport = require("passport");
 const localStrategy = require("passport-local");
 const User = require("./models/user");
 
-const campgrounds = require("./routes/campgrounds");
-const reviews = require("./routes/reviews");
+const userRoutes = require("./routes/users");
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
 
 mongoose.connect("mongodb://localhost:27017/campground", {
   useNewUrlParser: true,
@@ -61,17 +62,9 @@ app.use((req, res, next) => {
   next();
 })
 
-app.get("/fakeUser", async (req, res) => {
-  const user = new User({
-    email: "apple123@yahoo.com",
-    username: "core"
-  })
-  const newUser = await User.register(user, "hunter");
-  res.send(newUser)
-})
-
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
 app.get("/", (req, res) => {
   res.render("home");
