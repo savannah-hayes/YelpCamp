4e3cc42a62c8a6767b3c00dd2543f505b7b6596d
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2022-01-01 20:28:25 +0100

    Add image upload to cloudinary

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,12 +3,17 @@ const router = express.Router();
 const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utilities/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
-
-const Campground = require('../models/campground');
+const multer = require("multer");
+const { storage } = require("../cloudinary");
+const upload = multer({ storage })
 
 router.route("/")
   .get(catchAsync(campgrounds.index))
-  .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+  // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+  .post(upload.array("image"), (req, res) => {
+    console.log(req.body, req.files)
+    res.send("It worked")
+  })
 
 router.get("/new", isLoggedIn, campgrounds.newForm);
 
