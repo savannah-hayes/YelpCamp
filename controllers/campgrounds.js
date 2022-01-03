560f7c8867b1ec13d68a5461d6634072e01b89b3
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2022-01-03 19:16:28 +0100

    Add geocoding to locations

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,7 @@
 const Campground = require("../models/campground");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 const { cloudinary } = require("../cloudinary");
 
 const index = async (req, res, next) => {
@@ -11,7 +14,12 @@ const newForm = (req, res) => {
 };
 
 const createCampground = async (req, res, next) => {
+  const geoData = await geocoder.forwardGeocode({
+    query: req.body.campground.location,
+    limit: 1
+  }).send()
   const campground = new Campground(req.body.campground);
+  campground.geometry = geoData.body.features[0].geometry;
   campground.images = req.files.map(file => ({ url: file.path, filename: file.filename }));
   campground.author = req.user._id;
   await campground.save();
--- models/campground.js
+++ models/campground.js
@@ -2,14 +2,31 @@ const mongoose = require("mongoose");
 const Review = require("./review");
 const Schema = mongoose.Schema;
 
+// https://res.cloudinary.com/demo/image/upload/c_crop,g_face,h_400,w_400/r_max/c_scale,w_200/lady.jpg
+
+const ImageSchema = new Schema({
+  url: String,
+  filename: String
+});
+
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
+});
+
 const CampgroundSchema = new Schema({
   title: String,
-  images: [
-    {
-      url: String,
-      filename: String
+  images: [ImageSchema],
+  geometry: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      requried: true
     }
-  ],
+  },
   price: Number,
   description: String,
   location: String,
