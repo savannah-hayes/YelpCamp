7b5c32e991a64049f9b45573c6b1bce3c3552c42
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2022-01-10 22:07:14 +0100

    Update cluster map code

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,78 +1,78 @@
   mapboxgl.accessToken = mapToken;
   const map = new mapboxgl.Map({
-    container: 'map',
-  style: 'mapbox://styles/mapbox/dark-v10',
+    container: "map",
+  style: "mapbox://styles/mapbox/dark-v10",
   center: [-103.5917, 40.6699],
   zoom: 3
 });
  
-map.on('load', () => {
-    map.addSource('earthquakes', {
-      type: 'geojson',
-      data: 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+map.on("load", () => {
+    map.addSource("campgrounds", {
+      type: "geojson",
+      data: campgrounds,
       cluster: true,
       clusterMaxZoom: 14,
       clusterRadius: 50
     });
 
   map.addLayer({
-    id: 'clusters',
-  type: 'circle',
-  source: 'earthquakes',
-  filter: ['has', 'point_count'],
+    id: "clusters",
+  type: "circle",
+    source: "campgrounds",
+  filter: ["has", "point_count"],
   paint: {
-    'circle-color': [
-  'step',
-  ['get', 'point_count'],
-  '#51bbd6',
-  100,
-  '#f1f075',
-  750,
-  '#f28cb1'
-  ],
-  'circle-radius': [
-  'step',
-  ['get', 'point_count'],
-  20,
-  100,
+    "circle-color": [
+  "step",
+  ["get", "point_count"],
+  "#7db0de",
+  10,
+  "#1d5c96",
   30,
-  750,
-  40
+  "#12395d"
+  ],
+  "circle-radius": [
+  "step",
+  ["get", "point_count"],
+  15,
+  10,
+  20,
+  30,
+  25
   ]
 }
 });
 
   map.addLayer({
-    id: 'cluster-count',
-  type: 'symbol',
-  source: 'earthquakes',
-  filter: ['has', 'point_count'],
+    id: "cluster-count",
+  type: "symbol",
+    source: "campgrounds",
+  filter: ["has", "point_count"],
   layout: {
-    'text-field': '{point_count_abbreviated}',
-  'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
-  'text-size': 12
+    "text-field": "{point_count_abbreviated}",
+  "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+  "text-size": 12
 }
 });
 
   map.addLayer({
-    id: 'unclustered-point',
-  type: 'circle',
-  source: 'earthquakes',
-  filter: ['!', ['has', 'point_count']],
+    id: "unclustered-point",
+  type: "circle",
+    source: "campgrounds",
+  filter: ["!", ["has", "point_count"]],
   paint: {
-    'circle-color': '#11b4da',
-  'circle-radius': 4,
-  'circle-stroke-width': 1,
-  'circle-stroke-color': '#fff'
+    "circle-color": "#11b4da",
+  "circle-radius": 4,
+  "circle-stroke-width": 1,
+  "circle-stroke-color": "#fff"
 }
 });
 
-map.on('click', 'clusters', (e) => {
+map.on("click", "clusters", (e) => {
 const features = map.queryRenderedFeatures(e.point, {
-    layers: ['clusters']
+    layers: ["clusters"]
 });
   const clusterId = features[0].properties.cluster_id;
-  map.getSource('earthquakes').getClusterExpansionZoom(
+  map.getSource("campgrounds").getClusterExpansionZoom(
   clusterId,
 (err, zoom) => {
 if (err) return;
@@ -85,11 +85,11 @@ if (err) return;
   );
 });
 
-map.on('click', 'unclustered-point', (e) => {
+map.on("click", "unclustered-point", (e) => {
 const coordinates = e.features[0].geometry.coordinates.slice();
   const mag = e.features[0].properties.mag;
   const tsunami =
-  e.features[0].properties.tsunami === 1 ? 'yes' : 'no';
+  e.features[0].properties.tsunami === 1 ? "yes" : "no";
 
 while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
     coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
@@ -103,10 +103,10 @@ while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
     .addTo(map);
 });
  
-map.on('mouseenter', 'clusters', () => {
-      map.getCanvas().style.cursor = 'pointer';
+map.on("mouseenter", "clusters", () => {
+      map.getCanvas().style.cursor = "pointer";
 });
-map.on('mouseleave', 'clusters', () => {
-      map.getCanvas().style.cursor = '';
+map.on("mouseleave", "clusters", () => {
+      map.getCanvas().style.cursor = "";
 });
 });
