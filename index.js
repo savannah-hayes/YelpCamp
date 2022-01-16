a6c19200619129681ca9da4d4014f6af1e9562ea
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2022-01-16 23:46:28 +0100

    Setup mongo atlas and store session information

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const passport = require("passport");
 const localStrategy = require("passport-local");
 const User = require("./models/user");
 const helmet = require("helmet");
+const MongoStore = require('connect-mongo')(session);
 
 const mongoSanitize = require("express-mongo-sanitize")
 
@@ -21,7 +22,9 @@ const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 
-mongoose.connect("mongodb://localhost:27017/campground", {
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
@@ -41,16 +44,29 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(mongoSanitize());
+app.use(mongoSanitize({replaceWith: '_'}));
+
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
+const store = new MongoStore({
+  url: dbUrl,
+  secret,
+  touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function (e) {
+  console.log("SESSION STORE ERROR", e)
+})
 
 const sessionConfig = {
+  store,
   name: "session",
-  secret: "this should be better",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    // secure; true,
+    // secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7
   }
@@ -61,26 +77,26 @@ app.use(flash());
 app.use(helmet());
 
 const scriptSrcUrls = [
-  "https://stackpath.bootstrapcdn.com/",
-  "https://api.tiles.mapbox.com/",
-  "https://api.mapbox.com/",
-  "https://kit.fontawesome.com/",
-  "https://cdnjs.cloudflare.com/",
+  "https://stackpath.bootstrapcdn.com",
+  "https://api.tiles.mapbox.com",
+  "https://api.mapbox.com",
+  "https://kit.fontawesome.com",
+  "https://cdnjs.cloudflare.com",
   "https://cdn.jsdelivr.net",
 ];
 const styleSrcUrls = [
-  "https://kit-free.fontawesome.com/",
-  "https://stackpath.bootstrapcdn.com/",
-  "https://api.mapbox.com/",
-  "https://api.tiles.mapbox.com/",
-  "https://fonts.googleapis.com/",
-  "https://use.fontawesome.com/",
+  "https://kit-free.fontawesome.com",
+  "https://stackpath.bootstrapcdn.com",
+  "https://api.mapbox.com",
+  "https://api.tiles.mapbox.com",
+  "https://fonts.googleapis.com",
+  "https://use.fontawesome.com",
 ];
 const connectSrcUrls = [
-  "https://api.mapbox.com/",
-  "https://a.tiles.mapbox.com/",
-  "https://b.tiles.mapbox.com/",
-  "https://events.mapbox.com/",
+  "https://api.mapbox.com",
+  "https://a.tiles.mapbox.com",
+  "https://b.tiles.mapbox.com",
+  "https://events.mapbox.com",
 ];
 const fontSrcUrls = [];
 app.use(
@@ -96,8 +112,8 @@ app.use(
         "'self'",
         "blob:",
         "data:",
-        "https://res.cloudinary.com/drnhfd7jz/", 
-        "https://images.unsplash.com/",
+        "https://res.cloudinary.com/drnhfd7jz", 
+        "https://images.unsplash.com",
       ],
       fontSrc: ["'self'", ...fontSrcUrls],
     },
